968622ef39788a8a86a898fda59ca1d6e72ceef4
rizky ananda <muhammad.ananda@vision.id>
2023-08-29 12:54:58 +0700

    add module sample-lib

--- app/src/main/java/com/example/sample_sdk_jitpack/MainActivity.kt
+++ app/src/main/java/com/example/sample_sdk_jitpack/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.sample_lib.Greetings
 import com.example.sample_sdk_jitpack.ui.theme.SamplesdkjitpackTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +24,7 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        Greetings.Hello("MAIN_ACTIVITY","Hello World!")
     }
 }
 
---
+++ sample-lib/src/main/java/com/example/sample_lib/Greetings.kt
@@ -0,0 +1,11 @@
+package com.example.sample_lib
+
+import android.util.Log
+
+object Greetings {
+
+    fun Hello(tag: String, message: String) {
+        Log.d(tag, message)
+    }
+
+}
